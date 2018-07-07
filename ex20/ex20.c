9816899b7710124ed34b1f76131b638bff855a8a
¨jquinno¨ <joaquinsd10@gmail.com>
2018-07-07 22:30:51 +0900

    Up to excercise 20

--- ex20/ex20.c
+++ ex20/ex20.c
@@ -1,3 +1,4 @@
+#include "dbg.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
@@ -139,8 +140,16 @@ void test_sorting(int *numbers, int count, compare_cb cmp)
  	printf("\n");
 
  	free(sorted);
-}
 
+ 	unsigned char *data = (unsigned char *)cmp;
+
+ 	for (i = 0; i < 25; i++){
+ 		printf("%02x:", data[i]);
+ 	}
+
+ 	printf("\n");
+
+}
 
 
 
@@ -162,7 +171,7 @@ int main(int argc, char *argv[])
 
 	test_sorting(numbers, count, sorted_order);
 	test_sorting(numbers, count, reverse_order);
-	//test_sorting(numbers, count, strange_order);
+	test_sorting(numbers, count, strange_order);
 
 	free(numbers);
 
